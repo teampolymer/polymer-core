46ec38c5affc639dda0851a2eb567f10f1a6fb85
TT432 <2437315224@qq.com>
2021-09-30 13:57:42 +0800

    :adhesive_bandage:

---
+++ src/main/java/com/nmmoc7/polymercore/IPolymerCore.java
@@ -0,0 +1,6 @@
+package com.nmmoc7.polymercore;
+
+import net.minecraft.block.ITileEntityProvider;
+
+public interface IPolymerCore extends ITileEntityProvider {
+}
---
+++ src/main/java/com/nmmoc7/polymercore/capability/ItemCapabilityProvider.java
@@ -0,0 +1,34 @@
+package com.nmmoc7.polymercore.capability;
+
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.Direction;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityInject;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.util.INBTSerializable;
+import net.minecraftforge.common.util.LazyOptional;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class ItemCapabilityProvider implements ICapabilityProvider, INBTSerializable<CompoundNBT> {
+    @CapabilityInject(ItemCapability.class)
+    public static Capability<ItemCapability> CAPABILITY = null;
+    LazyOptional<ItemCapability> CONTEXT = LazyOptional.of(ItemCapability::new);
+
+    @Nonnull
+    @Override
+    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+        return cap == CAPABILITY ? CONTEXT.cast() : LazyOptional.empty();
+    }
+
+    @Override
+    public CompoundNBT serializeNBT() {
+        return null;
+    }
+
+    @Override
+    public void deserializeNBT(CompoundNBT nbt) {
+
+    }
+}
