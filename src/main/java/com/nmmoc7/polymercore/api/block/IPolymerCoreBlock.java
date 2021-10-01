1ba7361e82d928f4d7c98ced6d535482c6c23eb9
TT432 <2437315224@qq.com>
2021-10-01 12:57:41 +0800

    :fire: 删除了废弃的方块

--- src/main/java/com/nmmoc7/polymercore/api/block/IPolymerCoreBlock.java
+++
@@ -1,6 +0,0 @@
-package com.nmmoc7.polymercore.api.block;
-
-import net.minecraft.block.ITileEntityProvider;
-
-public interface IPolymerCoreBlock extends ITileEntityProvider {
-}
--- src/main/java/com/nmmoc7/polymercore/common/RegisterHandler.java
+++ src/main/java/com/nmmoc7/polymercore/common/RegisterHandler.java
@@ -1,12 +1,10 @@
 package com.nmmoc7.polymercore.common;
 
 import com.nmmoc7.polymercore.PolymerCore;
-import com.nmmoc7.polymercore.common.block.PolymerCoreBlock;
 import com.nmmoc7.polymercore.common.block.PolymerCoreBlueprintBlock;
 import com.nmmoc7.polymercore.common.event.DefaultTileRegEvent;
 import com.nmmoc7.polymercore.common.item.Hammer;
 import com.nmmoc7.polymercore.common.item.TestBlueprintItem;
-import com.nmmoc7.polymercore.common.tileentity.PolymerCoreTileEntity;
 import com.nmmoc7.polymercore.common.tileentity.blueprint.PolymerCoreBlueprintTileEntity;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
@@ -20,14 +18,10 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class RegisterHandler {
-    public static final PolymerCoreBlock BLOCK = new PolymerCoreBlock();
     public static final PolymerCoreBlueprintBlock BLUEPRINT_BLOCK = new PolymerCoreBlueprintBlock();
-    public static final BlockItem ITEM = new BlockItem(BLOCK, new Item.Properties().group(PolymerItemGroup.INSTANCE));
     public static final BlockItem BLUEPRINT_ITEM = new BlockItem(BLUEPRINT_BLOCK, new Item.Properties().group(PolymerItemGroup.INSTANCE));
-    public static TileEntityType<PolymerCoreTileEntity> TILE;
     public static TileEntityType<PolymerCoreBlueprintTileEntity> BLUEPRINT_TILE;
 
-    public static final ResourceLocation BLOCK_NAME = new ResourceLocation(PolymerCore.MOD_ID, "polymer_core_block");
     public static final ResourceLocation BLUEPRINT_BLOCK_NAME = new ResourceLocation(PolymerCore.MOD_ID, "polymer_core_blueprint_block");
 
     public static final Hammer HAMMER = new Hammer();
@@ -36,7 +30,6 @@ public class RegisterHandler {
     @SubscribeEvent
     public static void onBlockRegister(RegistryEvent.Register<Block> event) {
         event.getRegistry().registerAll(
-            BLOCK.setRegistryName(BLOCK_NAME),
             BLUEPRINT_BLOCK.setRegistryName(BLUEPRINT_BLOCK_NAME)
         );
     }
@@ -44,7 +37,6 @@ public class RegisterHandler {
     @SubscribeEvent
     public static void onItemRegister(RegistryEvent.Register<Item> event) {
         event.getRegistry().registerAll(
-                ITEM.setRegistryName(BLOCK_NAME),
                 BLUEPRINT_ITEM.setRegistryName(BLUEPRINT_BLOCK_NAME),
                 HAMMER.setRegistryName(new ResourceLocation(PolymerCore.MOD_ID, "hammer")),
                 TEST_BLUEPRINT_ITEM.setRegistryName(new ResourceLocation(PolymerCore.MOD_ID, "test_blueprint"))
@@ -53,23 +45,17 @@ public class RegisterHandler {
 
     @SubscribeEvent
     public static void onTileTypeRegister(RegistryEvent.Register<TileEntityType<?>> event) {
-        TILE = TileEntityType.Builder
-                .create(PolymerCoreTileEntity::new, getTileBlocks())
-                .build(null);
         BLUEPRINT_TILE = TileEntityType.Builder
                 .create(PolymerCoreBlueprintTileEntity::new, getBlueprintTileBlocks())
                 .build(null);
 
         event.getRegistry().registerAll(
-                TILE.setRegistryName(new ResourceLocation(PolymerCore.MOD_ID, BLOCK_NAME.getPath() + "_tile")),
                 BLUEPRINT_TILE.setRegistryName(new ResourceLocation(PolymerCore.MOD_ID, BLUEPRINT_BLOCK_NAME.getPath() + "_tile"))
         );
     }
 
     @SubscribeEvent
     public static void onDefaultTileRegister(DefaultTileRegEvent event) {
-        if (event.getType() == TILE)
-            event.addAll(BLOCK);
         if (event.getType() == BLUEPRINT_TILE)
             event.addAll(BLUEPRINT_BLOCK);
     }
@@ -77,12 +63,6 @@ public class RegisterHandler {
     /**
      * 如果想使用自带的tileentity，请在这个事件注册
      */
-    static Block[] getTileBlocks() {
-        DefaultTileRegEvent result = new DefaultTileRegEvent(TILE);
-        ModLoader.get().postEvent(result);
-        return result.getList().toArray(new Block[0]);
-    }
-
     static Block[] getBlueprintTileBlocks() {
         DefaultTileRegEvent result = new DefaultTileRegEvent(BLUEPRINT_TILE);
         ModLoader.get().postEvent(result);
