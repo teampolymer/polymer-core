a6284a26de090a4590f780e12064a2d37fbca430
WarmthDawn <warmth06@sina.com>
2021-10-02 22:57:01 +0800

    :bug: 修复部分报错

--- src/main/java/com/nmmoc7/polymercore/PolymerCore.java
+++ src/main/java/com/nmmoc7/polymercore/PolymerCore.java
@@ -1,7 +1,6 @@
 package com.nmmoc7.polymercore;
 
 import com.nmmoc7.polymercore.common.block.TestBlock;
-import com.nmmoc7.polymercore.common.blueprint.BlueprintRegisterHandler;
 import com.nmmoc7.polymercore.common.item.TestItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -14,7 +13,6 @@ public class PolymerCore {
 
     public PolymerCore() {
         IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
-        BlueprintRegisterHandler.REGISTER.register(modBus);
         TestItem.ItemRegistry.ITEMS.register(modBus);
         TestBlock.BlockRegistry.BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
